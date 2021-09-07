de5d568904138f3b0602df107455e369a176a00f
mndabeni06 <mndabeni6@gmail.com>
2021-09-07 09:37:45 +0200

    updated chnages

--- create profile.js
+++ create profile.js
@@ -1,24 +1,28 @@
-function addProducts() {
-  fetch('https://lca-pointofsales.herokuapp.com//create_profile/', {
+function addProfile() {
+  fetch('http://127.0.0.1:5000/create_profile/', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-          product_name: document.getElementById('p_name').value,
-          product_type: document.getElementById('p_type').value,
-          product_price: document.getElementById('p_price').value,
-          product_description: document.getElementById('p_description').value,
-          product_image: document.querySelector('.imgurl').src
+          full_name: document.getElementById('full_name').value,
+          nickname: document.getElementById('nickname').value,
+          age: document.getElementById('age').value,
+          date_of_birth: document.getElementById('date_of_birth').value,
+          place_of_birth:document.getElementById('place_of_birth').value,
+          citizenship:document.getElementById('citizeship').value,
+          position:document.getElementById('position').value,
+          current_club:document.getElementById('current_club').value,
+          player_image: document.querySelector('image').src
 
       }),
       mode: 'cors'
   }).then(res => res.json()).then(data => {
       console.log(data)
-      console.log("You added a product successfully")
+      console.log("Profile created successfully")
 
       if (data['message'] == "Player Profile Added Successfully") {
-          alert('You have sucessfully Added A Product! Please View It On The Products Page')
+          alert('You have sucessfully Added A Profile! Please View It On The add player profile Page')
           window.location.href = './add player profile'
       } else {
           alert('Form filled in incorrectly')
@@ -27,7 +31,7 @@ function addProducts() {
 }
 
 function addImage() {
-  const preview = document.querySelector('.imgurl');
+  const preview = document.querySelector('.image');
   const file = document.querySelector('input[type=file]').files[0];
   const reader = new FileReader();
 
