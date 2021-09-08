3d2dd1aef71df9262d8bcf78364edd8c6c76b717
mndabeni06 <mndabeni6@gmail.com>
2021-09-08 13:17:46 +0200

    registration js

--- register.js
+++ register.js
@@ -1,7 +1,9 @@
 // function to register player
-/*function registerPlayer() {
+function registerPlayer() {
+  fetch("https://immense-coast-90376.herokuapp.com/player_reg/", {
+    method: "POST", 
+    body: JSON.stringify({
 
-  let details = {
     first_name: document.getElementById("first_name").value,
     last_name: document.getElementById("last_name").value,
     email: document.getElementById("email").value,
@@ -9,27 +11,21 @@
     password: document.getElementById("password").value,
     home_address: document.getElementById("home_address").value,
     contact_number: document.getElementById("contact_number").value,
-  }
-
-  console.log(details)
-    fetch("https://immense-coast-90376.herokuapp.com/player_reg/", {
-      method: "POST",
-      body: JSON.stringify(details),
-      headers: {
-        "Content-type": "application/json"
-        
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data["status_code"] == 200) {
-          alert("Registered successfully!, please log in.");
-          window.location.href = "./login.html";
-        } else {
-          alert("Please enter correct information");
-          
-        }
-      });
-  }*/
-
+  }),
+  headers: {
+    "Content-type": "application/json"
+    
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    if (data["status_code"] == 201) {
+      alert("Registered successfully!, please log in.");
+      window.location.href = "./login.html";
+    } else {
+      alert("Please enter correct information");
+      
+    }
+  });
+}
